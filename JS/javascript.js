dba33aae814ac4649d754d1bfb7344411d0cf037
leonmoreti <leonmoreti7@gmail.com>
2022-10-20 16:40:47 -0300

    first commit

---
+++ JS/javascript.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    var repeticao = 0;
+
+    //Precisamos de uma variável inicial.
+    /*
+    while(repeticao < 10){
+        console.log("Olá mundo número: " +repeticao);
+        repeticao++; //sem "repeticao++" o navegador trava.
+    }
+
+    console.log("Cheguei até o final!");
+}
+*/
+
+for(var numero = 0; numero < 10; numero++){
+    console.log("Olá mundo!");
+    }
+
+}
